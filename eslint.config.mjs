75da1401dc9daa4a4dcb4af08f41cb10d56dc3b8
Chico Venancio <francisco@argyle.com>
2024-07-09 20:20:51 -0300

    Fix eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,66 @@
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [{
+    ignores: ["**/node_modules", "**/dist", "**/coverage", "**/*.d.ts", "**/tests", "eslint.config.mjs", "**/.venv"],
+}, ...compat.extends(
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+), {
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
+    },
+
+    languageOptions: {
+        parser: tsParser,
+        ecmaVersion: 5,
+        sourceType: "module",
+
+        parserOptions: {
+            project: "tsconfig.json",
+        },
+    },
+
+    rules: {
+        "@typescript-eslint/naming-convention": ["error", {
+            selector: "interface",
+            format: ["PascalCase"],
+
+            custom: {
+                regex: "^I[A-Z]",
+                match: true,
+            },
+        }],
+
+        "@typescript-eslint/no-unused-vars": ["warn", {
+            args: "none",
+        }],
+
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-namespace": "off",
+        "@typescript-eslint/no-use-before-define": "off",
+
+        "@typescript-eslint/quotes": ["error", "single", {
+            avoidEscape: true,
+            allowTemplateLiterals: false,
+        }],
+
+        curly: ["error", "all"],
+        eqeqeq: "error",
+        "prefer-arrow-callback": "error",
+    },
+}];
